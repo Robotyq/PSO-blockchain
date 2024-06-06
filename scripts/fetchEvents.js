7408882451f31107654db07f9686142b36ca82f7
Robotyq <davdanioan@gmail.com>
2024-06-07 01:51:21 +0300

    fetch

--- scripts/fetchEvents.js
+++ scripts/fetchEvents.js
@@ -3,46 +3,7 @@ const Particle = artifacts.require("Particle");
 
 module.exports = async function (callback) {
     try {
-        const globalVar = await Controller.deployed();
-        // Fetch all events from the Controller contract
-        const globalVarEvents = await globalVar.getPastEvents('allEvents', {
-            fromBlock: 0,
-            toBlock: 'latest'
-        });
-        console.log('Controller Events:');
-        // Format and print each Controller event
-        globalVarEvents.forEach(event => {
-            switch (event.event) {
-                case 'UpdateVar':
-                    console.log(`UpdateVar Event:
-                                Particle: ${event.returnValues.particle}
-                                Old Best Position: ${event.returnValues.old.bestPos}
-                                Old Min Value: ${event.returnValues.old.minValue}
-                                New Best Position: ${event.returnValues.newVar.bestPos}
-                                New Min Value: ${event.returnValues.newVar.minValue}
-                                `);
-                    break;
-                case 'ParticleBorn':
-                    console.log(`ParticleBorn Event:
-                                Particle: ${event.returnValues.particle}
-                                Initial Position: ${event.returnValues.position}
-                                Speed: ${event.returnValues.speed}
-                                `);
-                    break;
-                case 'NewLocalMax':
-                    console.log(`NewLocalMax Event:
-                                Particle: ${event.returnValues.particle}
-                                Old Value: ${event.returnValues.old}
-                                New Value: ${event.returnValues.newVal}
-                                `);
-                    break;
-                default:
-                    console.log(`Unknown Event:
-                                ${JSON.stringify(event, null, 2)}
-                                `);
-                    break;
-            }
-        });
+        const controller = await Controller.deployed();
 
         // Assume there are exactly 7 particles
         const particleCount = 7;
@@ -50,7 +11,7 @@ module.exports = async function (callback) {
         console.log('Particle Events:');
         // Iterate over particle addresses and fetch their events
         for (let i = 0; i < particleCount; i++) {
-            const address = await globalVar.particleAddresses(i);
+            const address = await controller.particleAddresses(i);
             const particleInstance = await Particle.at(address);
 
             // Fetch all events from the Particle contract
@@ -62,19 +23,16 @@ module.exports = async function (callback) {
             // Format and print each Particle event
             particleEvents.forEach(event => {
                 switch (event.event) {
-                    case 'NewLocalMax':
-                        console.log(`NewLocalMax Event from Particle at ${address}:
-                                    Old Best Position: ${event.returnValues.old.bestPos}
-                                    Old Min Value: ${event.returnValues.old.minValue}
-                                    New Best Position: ${event.returnValues.newVal.bestPos}
-                                    New Min Value: ${event.returnValues.newVal.minValue}
+                    case 'NewLocalMin':
+                        console.log(`NewLocalMin Event from Particle at ${address}:
+                                    Old Position: ${event.returnValues.old.x}, ${event.returnValues.old.y}, ${event.returnValues.old.z}
+                                    New Position: ${event.returnValues.newVal.x}, ${event.returnValues.newVal.y}, ${event.returnValues.newVal.z}
                                     `);
                         break;
-
                     default:
                         console.log(`Unknown Event from Particle at ${address}:
-              ${JSON.stringify(event, null, 2)}
-            `);
+                                    ${JSON.stringify(event, null, 2)}
+                                    `);
                         break;
                 }
             });
@@ -83,5 +41,5 @@ module.exports = async function (callback) {
     } catch (error) {
         console.error(error);
         callback(error);
-    }await globalVar.iterateAll
+    }
 };
