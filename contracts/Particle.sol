ab4ca1b797184c41035c9942e27c29b884089da6
Robotyq <davdanioan@gmail.com>
2024-06-06 18:06:08 +0300

    particle

--- contracts/Particle.sol
+++ contracts/Particle.sol
@@ -13,15 +13,13 @@ import "./IFunction.sol";
         int y;
         int z;
     }
-
 contract Particle {
-    uint private constant dimension = 2;
     int[dimension] public position;
     IFunction public targetFunction;
     int[dimension + 1] public localBest;
     int private localMin = 999999999999;
     int[dimension] private speed;
-    int private nonce = 0;
+    uint private nonce = 0;
     IController private controller;
 
     event NewLocalMin(address particle, Point3 old, Point3 newVal);
@@ -31,7 +29,6 @@ contract Particle {
         targetFunction = IFunction(_TargetContractAddress);
         position = initialPos;
         speed = initialVelocity;
-        localBest = new int[](dimension + 1);
         for (uint i = 0; i < dimension; i++) {
             localBest[i] = position[i];
         }
@@ -46,31 +43,30 @@ contract Particle {
         int newVal = findNewVal(cognitiveFactor, socialFactor, 33, globalBest);
         if (newVal < localBest[dimension]) {
             int[dimension + 1] memory old = localBest;
-            for (uint i = 0; i < dimension - 1; i++) {
+            for (uint i = 0; i < dimension; i++) {
                 localBest[i] = position[i];
             }
             localBest[dimension] = newVal;
-            emit NewLocalMin(msg.sender, old, localBest);
+            emit NewLocalMin(msg.sender, Point3(old[0], old[1], old[2]), Point3(localBest[0], localBest[1], localBest[2]));
             if (newVal < globalBest[dimension]) {
                 controller.setBestPoint(localBest);
             }
         }
     }
 
-    function findNewVal(int cogF, int socialF, int inertiaF, int[dimension] memory globalBest) private returns (int){
-        int[dimension + 1] calldata newSpeed;
-        uint maxSpeed = 30;
+    function findNewVal(int cogF, int socialF, int inertiaF, int[dimension + 1] memory globalBest) private returns (int) {
+        int[dimension] memory newSpeed;
+        uint8 maxSpeed = 30;
         for (uint i = 0; i < dimension; i++) {
             int newSpeedi = speed[i] * inertiaF / 100 + cogF * (localBest[i] - position[i]) / 100 + socialF * (globalBest[i] - position[i]) / 100;
 
             if (newSpeedi > maxSpeed) {
                 newSpeedi = maxSpeed;
-            } else if (newSpeedi < - maxSpeed) {
-                newSpeedi = - maxSpeed;
+            } else if (newSpeedi < -maxSpeed) {
+                newSpeedi = -maxSpeed;
             }
 
             newSpeed[i] = newSpeedi;
-
             position[i] = position[i] + newSpeed[i];
         }
         speed = newSpeed;
@@ -78,18 +74,16 @@ contract Particle {
         return value;
     }
 
-
-    function random(int min, int max) private returns (int){
+    function random(int min, int max) private returns (int) {
         nonce++;
-        uint rand = uint(keccak256(abi.encodePacked(block.blockhash(), this, nonce)));
+        uint rand = uint(keccak256(abi.encodePacked(blockhash(block.number - 1), msg.sender, nonce)));
         int randi = int(rand);
         int interval = max - min;
-        return int(randi % interval + min);
+        return randi % interval + min;
     }
 }
 
 interface IController {
-    function getBestPoint() external view returns (int[3] calldata);
-
-    function setBestPoint(int[3] calldata newVar) external;
+    function getBestPoint() external view returns (int[3] memory);
+    function setBestPoint(int[3] memory newVar) external;
 }
