91ac902ae6f570ba3b1d04aeca4edf2624120585
Robotyq <davdanioan@gmail.com>
2024-06-11 17:49:10 +0300

    ifunc

---
+++ contracts/IFunction.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+uint constant dimension = 2;
+interface IFunction {
+    function compute(int[dimension] calldata positions) external view returns (int);
+
+    function getFactor() external view returns (int);
+
+    function setFactor(int _factor) external;
+
+    event FunctionContractDeployed(address thisContractAddress);
+}
