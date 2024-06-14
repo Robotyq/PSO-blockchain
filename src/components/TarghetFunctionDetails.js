009ddef417357e1d73777f0347fdd7bbaeadc8ab
Robotyq <davdanioan@gmail.com>
2024-06-14 20:44:53 +0300

    latex

--- src/components/TarghetFunctionDetails.js
+++ src/components/TarghetFunctionDetails.js
@@ -1,30 +1,29 @@
 import React from 'react';
+import Latex from 'react-latex-next';
 import styles from './TargetFunctionDetails.module.css';
 import {functionNames} from "@/components/TargetFunctionSelector";
 
 const TargetFunctionDetails = ({targetFunction}) => {
-    console.log('Targhet Function Details...', targetFunction);
     const funcName = functionNames[targetFunction.slice(-3).toLowerCase()];
-    console.log('Function Name...', funcName);
 
     const functionDetails = {
         'Rastrigin': {
             name: 'Rastrigin Function',
-            formula: 'An + Σ[x_i^2 - A * cos(2π * x_i)]',
+            formula: 'A n + \\sum_{i=1}^{n} \\left[ x_i^2 - A \\cos(2 \\pi x_i) \\right]',
             description: 'The Rastrigin function is a non-convex function used as a performance test problem for optimization algorithms. It is highly multimodal, but locations of the minima are regularly distributed.',
-            graphic: '/path/to/rastrigin.png'
+            graphic: '/images/rastrigin.png'
         },
         'Rosenbrock': {
             name: 'Rosenbrock Function',
-            formula: 'Σ [100 * (x_{i+1} - x_i^2)^2 + (1 - x_i)^2]',
+            formula: '\\sum_{i=1}^{n-1} \\left[ 100 (x_{i+1} - x_i^2)^2 + (1 - x_i)^2 \\right]',
             description: 'The Rosenbrock function, also known as the Valley or Banana function, is a popular test problem for optimization algorithms. The global minimum is inside a long, narrow, parabolic shaped flat valley.',
-            graphic: '/path/to/rosenbrock.png'
+            graphic: '/images/rosenbrock.png'
         },
         'Sphere Function': {
             name: 'Sphere Function',
-            formula: 'Σ (x_i - a)^2 + (y_i - b)^2',
+            formula: '\\sum_{i=1}^{n} (x_i - a)^2 + (y_i - b)^2',
             description: 'The Sphere function is a simple unimodal function. It is continuous, convex, and unimodal. The global minimum is at the origin.',
-            graphic: '/path/to/sphere.png'
+            graphic: '/images/sphere.png'
         }
     };
 
@@ -34,7 +33,8 @@ const TargetFunctionDetails = ({targetFunction}) => {
         <div className={styles.functionDetails}>
             <h2>{details.name}</h2>
             <p><strong>Formula:</strong></p>
-            <p>{details.formula} </p>
+            {/*<p><Latex>{`$${details.formula}$`}</Latex></p>*/}
+            <Latex>{`$${details.formula}$`}</Latex>
             <p>{details.description}</p>
             {details.graphic && <img src={details.graphic} alt={`${details.name} graphic`} className={styles.graphic}/>}
         </div>
